3d14ddf3f38fa98b5fcbf7b92ef120651e9364be
adsznzhang <adszn@hotmail.com>
2017-01-01 15:16:37 +0800

    Update global.js

--- final project/scripts/global.js
+++ final project/scripts/global.js
@@ -1,1 +1,34 @@
+//添加几个在整个站点都会用到的函数！
+function addLoadEvent(func){
+  var oldonload = window.onload;
+  if(typeof window.onload != "function"){
+    window.onload = func;
+  }else {
+    window.onload = function(){
+      oldonload();
+      func();
+    }
+  }
+}
 
+//insertAfter 也很有用
+function inserAfter(newElement, targetElement) {
+  var parent = targetElement.parentNode;
+  if(parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  }else{
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
+}
+//最后是addClass函数
+
+function addClass(element,value) {
+  if(!element.className){
+    element.className = value;
+  }else {
+    newClassName = element.className;
+    newClassName+= " "
+    newClassName+= value;
+    element.className = newClassName;
+  }
+}
