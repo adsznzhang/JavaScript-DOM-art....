86054dad134eacc3ccc2e92ad06e3991b732c7e6
adsznzhang <adszn@hotmail.com>
2017-01-01 19:35:09 +0800

    Update global.js

--- final project/scripts/global.js
+++ final project/scripts/global.js
@@ -32,3 +32,22 @@ function addClass(element,value) {
     element.className = newClassName;
   }
 }
+//页面突出函数
+function highlightPage() {
+  if(!document.getElementsByTagName) return false;
+  if(!document.getElementById) return false;
+  var headers = document.getElementsByTagName("header");
+  if(headers.length == 0) return false;
+  var navs = headers[0].getElementsByTagName("nav");
+  if(navs.length == 0) return false;
+  var links = navs[0].getElmentsByTagName("a");
+//这有吗？  for(var i = 0;i < links.length; i++){
+    var linkurl;
+    for(var i = 0; i<links.length; i++){
+      linkurl = links[i].getAttribute("href");
+      if(window.location.href.indexOf(linkurl) != -1){
+        links[i].className = "here";
+      }
+    }
+}
+
